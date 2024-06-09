d80ede4f71c10da14afc1e2f863447d092038abc
Diego Peroza <diegoeleveth@gmail.com>
2024-06-09 21:38:59 +0200

    Agregado font Awesome

--- app_lista_tareas/src/componentes/Header.js
+++ app_lista_tareas/src/componentes/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
     return ( 
         <header className='header'>
             <h1 className='header__titulo'>Lista de Tareas</h1>
             <button className='header__boton'>
                 No mostrar completadas
+                <FontAwesomeIcon icon={faEyeSlash} className="header__icono-boton" />
             </button>
         </header>
      );
