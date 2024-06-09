8cde305c111602550441dea3ab1c43ff65967447
Diego Peroza <diegoeleveth@gmail.com>
2024-06-09 23:24:06 +0200

    Formulario install uuid

--- app_lista_tareas/src/App.js
+++ app_lista_tareas/src/App.js
@@ -1,12 +1,30 @@
+import React, {useState} from 'react';
 import './App.css';
 import FormularioTareas from './componentes/FormularioTareas';
 import Header from './componentes/Header'
 
 const App = () => {
+  const [tareas, cambiarTareas] = useState(
+    [
+      {
+        id: 1,
+        texto: 'Laver la ropa',
+        completada: false
+      },
+      {
+        id: 2,
+        texto: 'Grabar tutorial',
+        completada: false
+      }
+    ]
+  );
+
+  console.log(tareas);
+
   return (
     <div className="contenedor">
       <Header />
-      <FormularioTareas />
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
     </div>
   );
 }
--- app_lista_tareas/src/componentes/FormularioTareas.js
+++ app_lista_tareas/src/componentes/FormularioTareas.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPlusSquare} from '@fortawesome/free-solid-svg-icons';
 
-const FormularioTareas = () => {
+const FormularioTareas = ({tareas, cambiarTareas}) => {
+    const [inputTarea, cambiarInputTareas] = useState('');
+
+    const handleInput = (e) =>{
+        cambiarInputTareas(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        cambiarTareas(
+            [
+                ... tareas, 
+                {
+                    id: uuidv4(),
+                    texto: inputTarea,
+                    completada: false
+                }
+            ]
+        );
+    }
+
     return ( 
-        <form action="" className="formulario-tareas">
+        <form action="" className="formulario-tareas" onSubmit={handleSubmit}>
             <input 
                 type="text" 
                 className="formulario-tareas__input"
                 placeholder="Escribe una tarea"
+                value={inputTarea}
+                onChange={(e) => handleInput(e)}
                 />
             <button 
                 type="submit"
