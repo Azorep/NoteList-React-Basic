51da21ab1372d54dd010484202bc145bc5b27992
Diego Peroza <diegoeleveth@gmail.com>
2024-06-10 16:33:11 +0200

    Fomulario para editar tareas

--- app_lista_tareas/src/componentes/Tarea.js
+++ app_lista_tareas/src/componentes/Tarea.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCheckSquare, faEdit, faTimes} from '@fortawesome/free-solid-svg-icons';
 
 const Tarea = ({tarea}) => {
+    const [editandoTarea, cambiarEditandoTarea] = useState(false);
+    const [nuevaTarea, cambiarNuevaTarea] = useState(tarea.texto);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        cambiarEditandoTarea(false);
+    }
+
     return ( 
         <li className="lista-tareas__tarea">
             <FontAwesomeIcon 
@@ -10,12 +18,28 @@ const Tarea = ({tarea}) => {
                 className="lista-tareas__icono lista-tareas__icono-check"
             />
             <div className="lista-tareas__texto">
-                {tarea.texto}
+                {editandoTarea ? 
+                <form action="" className="formulario-editar-tarea" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        className="formulario-editar-tarea__input"
+                        value={nuevaTarea}
+                        onChange={(e) => cambiarNuevaTarea(e.target.value)}
+                    />
+                    <button type="submit" 
+                    className="formulario-editar-tarea__btn"
+                    >
+                        Actualizar
+                    </button>
+                </form>
+                : tarea.texto  
+                }
             </div>
             <div className="lista-tareas__contenedor-botones">
                 <FontAwesomeIcon 
                     icon={faEdit}
                     className="lista-tareas__icono lista-tareas__icono-accion"
+                    onClick={() => cambiarEditandoTarea(true)}
                 />
                  <FontAwesomeIcon 
                     icon={faTimes}
